0fddd4997f1cbaf9c0dc85f5bbf7730323de373d
mohammed Hasif <hazif1998@gmail.com>
2024-06-20 21:55:15 +0400

    brototype_flutter_part7

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 main() {
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Color.fromARGB(255, 236, 3, 3),
+        primaryColor: Color.fromARGB(255, 131, 236, 3),
       ),
       home: Homescreen(),
     );
@@ -24,8 +25,96 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(236, 187, 255, 1),
-      appBar: AppBar(),
+      backgroundColor: Color.fromARGB(235, 198, 166, 157),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: const Text('My App is bulid wi', textAlign: TextAlign.right),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.purple,
+              child: Column(
+                children: [
+                  const Text(
+                    'Hasif',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        print("button printed");
+                      },
+                      child: Text('Click me')),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            print("elevated button printed");
+                          },
+                          child: Text("elevated button")),
+                      IconButton(
+                          onPressed: () {
+                            print("icon button printed");
+                          },
+                          icon: Icon(Icons.ice_skating)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(30)))),
+          Expanded(
+            child: Container(
+              color: Colors.lightBlue,
+              child: Column(
+                children: [
+                  Text(
+                    'Hasif',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        print("button printed");
+                      },
+                      child: Text('Click me')),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {
+                            print("elevated button printed");
+                          },
+                          child: Text("elevated button")),
+                      IconButton(
+                          onPressed: () {
+                            print("icon button printed");
+                          },
+                          icon: Icon(Icons.ice_skating)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
